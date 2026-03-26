8b086484b1ca4addb6ae5e0294b3d4b9cb55991b
Sharon Aicler <saichler@gmail.com>
2026-03-26 05:54:08 -0700

    Update event service

--- go/services/EventService.go
+++ go/services/EventService.go
@@ -16,11 +16,15 @@
 package services
 
 import (
+	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
+	_ "github.com/lib/pq"
 	evt "github.com/saichler/l8events/go/types/l8events"
-	"github.com/saichler/l8services/go/services/base"
+	"github.com/saichler/l8orm/go/orm/persist"
+	"github.com/saichler/l8orm/go/orm/plugins/postgres"
 	"github.com/saichler/l8types/go/ifs"
 	"github.com/saichler/l8types/go/types/l8api"
 	"github.com/saichler/l8types/go/types/l8web"
@@ -32,22 +36,49 @@ const (
 	EventsServiceArea = byte(76)
 )
 
-func ActivateEvents(vnic ifs.IVNic) {
-	serviceConfig := ifs.NewServiceLevelAgreement(&base.BaseService{}, EventsServiceName, EventsServiceArea, true, &EventCallback{})
-	serviceConfig.SetServiceGroup(ifs.SystemServiceGroup)
-	serviceConfig.SetServiceItem(&evt.EventRecord{})
-	serviceConfig.SetServiceItemList(&evt.EventRecordList{})
-	serviceConfig.SetVoter(true)
-	serviceConfig.SetTransactional(false)
-	serviceConfig.SetPrimaryKeys("EventId")
+func ActivateEvents(creds, dbname string, vnic ifs.IVNic) {
+	realdb, user, pass, _, err := vnic.Resources().Security().Credential(creds, dbname, vnic.Resources())
+	if err != nil {
+		panic(err)
+	}
+	db := openEventsDB(realdb, user, pass)
+	p := postgres.NewPostgres(db, vnic.Resources())
+
+	sla := ifs.NewServiceLevelAgreement(&persist.OrmService{}, EventsServiceName, EventsServiceArea, true, &EventCallback{})
+	sla.SetServiceGroup(ifs.SystemServiceGroup)
+	sla.SetServiceItem(&evt.EventRecord{})
+	sla.SetServiceItemList(&evt.EventRecordList{})
+	sla.SetVoter(true)
+	sla.SetPrimaryKeys("EventId")
+	sla.SetArgs(p, true)
+	sla.SetTransactional(true)
+	sla.SetReplication(true)
+	sla.SetReplicationCount(3)
 
 	webSv := web.New(EventsServiceName, EventsServiceArea, 0)
 	webSv.AddEndpoint(&evt.EventRecord{}, ifs.POST, &l8web.L8Empty{})
 	webSv.AddEndpoint(&evt.EventRecord{}, ifs.PATCH, &l8web.L8Empty{})
 	webSv.AddEndpoint(&l8api.L8Query{}, ifs.GET, &evt.EventRecordList{})
-	serviceConfig.SetWebService(webSv)
+	sla.SetWebService(webSv)
 
-	base.Activate(serviceConfig, vnic)
+	vnic.Resources().Services().Activate(sla, vnic)
+}
+
+func openEventsDB(dbname, user, pass string) *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		"127.0.0.1", 5432, user, pass, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(fmt.Errorf("failed to connect to events database: %w", err))
+	}
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(5 * time.Minute)
+	return db
 }
 
 type EventCallback struct{}
