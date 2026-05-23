e36c5ec00709c043ba632ccbc39c835c1a055187
Sharon Aicler <saichler@gmail.com>
2026-05-23 12:17:42 -0700

    Remove api

--- go/services/EventService.go
+++ go/services/EventService.go
@@ -20,10 +20,10 @@ import (
 	"github.com/saichler/l8events/go/common"
 	"time"
 
-	evt "github.com/saichler/l8types/go/types/l8events"
 	"github.com/saichler/l8orm/go/orm/persist"
 	"github.com/saichler/l8types/go/ifs"
 	"github.com/saichler/l8types/go/types/l8api"
+	evt "github.com/saichler/l8types/go/types/l8events"
 	"github.com/saichler/l8types/go/types/l8web"
 	"github.com/saichler/l8utils/go/utils/web"
 )
